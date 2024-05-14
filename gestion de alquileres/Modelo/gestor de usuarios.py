efb82d6bb9562bdd6a431883ef87f625f7ed83e6
JennS23 <jennscaddei2003@gmail.com>
2024-05-13 19:17:28 -0500

    creacion de los gestores

---
+++ gestion de alquileres/Modelo/gestor de usuarios.py
@@ -0,0 +1,25 @@
+#Gestor de usuario:
+
+class User:
+    def __init__(self, user_id, password):
+        self.user_id = user_id
+        self.password = password
+
+class UserManager:
+    def __init__(self):
+        self.users = {}
+
+    def register_user(self, user_id, password):
+        if user_id in self.users:
+            return "El usuario ya existe. Por favor, elige otro ID."
+        else:
+            self.users[user_id] = User(user_id, password)
+            return "Usuario registrado exitosamente."
+
+    def delete_user(self, user_id):
+        if user_id in self.users:
+            del self.users[user_id]
+            return "Usuario eliminado exitosamente."
+        else:
+            return "El usuario no existe."
+
---
+++ gestion de alquileres/Modelo/gestor de vivienda.py
@@ -0,0 +1,23 @@
+class House:
+    def __init__(self, address, name, rent_price):
+        self.address = address
+        self.name = name
+        self.rent_price = rent_price
+
+class HouseManager:
+    def __init__(self):
+        self.houses = {}
+
+    def add_house(self, address, name, rent_price):
+        if name in self.houses:
+            return False  # La casa ya existe
+        else:
+            self.houses[name] = House(address, name, rent_price)
+            return True  # Casa añadida correctamente
+
+    def remove_house(self, name):
+        if name in self.houses:
+            del self.houses[name]
+            return True  # Casa eliminada correctamente
+        else:
+            return False  # La casa no existe
